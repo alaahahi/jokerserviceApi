0274e0b1b36f1be731f6e02580a1c7a3adbeb9ff
alaahahi <alaa.hahe@gmail.com>
2021-07-26 01:17:33 +0300

    Update employees_accept.blade.php

--- resources/views/employees_accept.blade.php
+++ resources/views/employees_accept.blade.php
@@ -27,12 +27,14 @@ Employees Mobile: {{ $customer->phone }}
     <p>Accepted : {{ $customer->is_active }}</p>
     <div class="card-footer text-center">
           <div class="btn-wrapper  justify-content-between">
-          @if ($customer->is_active==0)
+          @if ($customer->is_active==0 && $customer->is_block==0)
           <a href="javascript:void(0)" data-toggle="tooltip"    data-id="{{$customer->id}}"  class="btn btn-danger rejection">Rejection</a>
           <a href="javascript:void(0)" data-toggle="tooltip"  id="{{$customer->id}}"  data-id="{{$customer->id}}" class="btn btn-warning approval">Approval</a>
+          <a href="javascript:void(0)" data-toggle="tooltip"  data-id="{{$customer->id}}"   class="btn btn-info blocked {{$customer->id}}">Block</a>
           @endif
-          @if ($customer->is_active==1)
+          @if ($customer->is_active==1 && $customer->is_block==0)
           <a href="javascript:void(0)" data-toggle="tooltip"     class="btn btn-success ">Accepted </a>
+          <a href="javascript:void(0)" data-toggle="tooltip"  data-id="{{$customer->id}}"   class="btn btn-info blocked {{$customer->id}}">Block</a>
           @endif
           @if ($customer->is_blocked==1)
           <a href="javascript:void(0)" data-toggle="tooltip"  id="{{$customer->id}}"  data-id="{{$customer->id}}"   class="btn btn-danger un_block">Un Block</a>
@@ -65,6 +67,14 @@ Employees Mobile: {{ $customer->phone }}
       $.get("{{ route('block_employee') }}/"+Item_id ).done(function() {
         $('#'+Item_id).attr('disabled', false); 
         $('#'+Item_id).css("background-color", "#2ecc71").text("Done Rejection");
+});
+   });
+   $('body').on('click', '.blocked', function () {
+      $(this).attr('disabled', true);      
+      var Item_id = $(this).data('id');
+      $.get("{{ route('block_employee') }}/"+Item_id ).done(function() {
+        $('.'+Item_id).attr('disabled', false); 
+        $('.'+Item_id).css("background-color", "#2ecc71").text("Done"); 
 });
    });
    $('body').on('click', '.un_block', function () {
